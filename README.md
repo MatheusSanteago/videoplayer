711d9e7fa592070b7e800e85327998d2ab7a2ac5
MatheusSanteago <matheussanteago@gmail.com>
2022-03-07 21:05:54 -0300

    edit 07/03

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 <h1> Music Player </h1>
+<img src="/Assets/x.png">
--- Video/main.js
+++ Video/main.js
@@ -1,41 +1,40 @@
+var video = document.getElementById("video");
+let spanVideo = document.getElementById("speed");
+let divControls = document.getElementById("controls");
+let container = document.getElementById("container");
+let slider = document.getElementById("progress");
 
-var video = document.getElementById('video');
-let spanVideo = document.getElementById('speed');
-let divControls = document.getElementById('controls');
-let container = document.getElementById('container');
-let slider = document.getElementById('progress');
 
-video.ontimeupdate = getTime;    
+video.ontimeupdate = getTime;
 slider.onchange = getSliderTime;
+
+container.addEventListener("mouseout", hideControls);
+container.addEventListener("mouseover", showControls);
 
 
 function getTime() {
-    let tempo = (video.currentTime);
+    let tempo = video.currentTime;
     slider.value = tempo;
-    spanVideo.innerHTML = Math.ceil(tempo);
-    console.log(slider);
+    spanVideo.innerHTML = Math.ceil(tempo) + 's';
 }
 function getSliderTime() {
-    video.currentTime = slider.value
+    video.currentTime = slider.value;
 }
-function videoSet(){
-    var inputSRC = document.getElementById('videosrc').value;
-    video.setAttribute("src",inputSRC);
+function videoSet() {
+    var inputSRC = document.getElementById("videosrc").value;
+    video.setAttribute("src", inputSRC);
 }
 
-function hideControls(){
+function hideControls() {
     divControls.style.visibility = "hidden";
 }
-function showControls(){
+function showControls() {
     divControls.style.visibility = "visible";
 }
-addEventListener('mouseout',hideControls);
-addEventListener('mouseover',showControls);
-
 function rewind() {
     video.currentTime -= 15;
 }
-function play(){
+function play() {
     video.play();
 }
 function pause() {
@@ -44,25 +43,49 @@ function pause() {
 function skip() {
     video.currentTime += 15;
 }
-function sound(){
+
+
+let soundLogo = document.getElementById('sound-logo');
+soundLogo.addEventListener('mouseover', sound);
+
+function sound() {
+    let verify = document.getElementById('soundbar');
+    if (verify) verify.remove()
+    let soundbar = document.createElement("div");
+    let soundbarRange = document.createElement("input");
+    divControls.appendChild(soundbar);
+    soundbar.appendChild(soundbarRange);
+    soundbar.setAttribute("id", "soundbar");
+    soundbarRange.setAttribute("type", "range");
+    soundbarRange.setAttribute("id", "volume");
+    soundbarRange.setAttribute("class", "slider");
+    soundbarRange.setAttribute('value', '1')
+    soundbarRange.setAttribute('max', '1')
+    soundbarRange.setAttribute('step', '0.1')
+    soundbarRange.setAttribute('onchange', 'soundLevel()')
 
 }
-function speedUp(){
-    let a = video.playbackRate += 0.5;
+function soundLevel() {
+    var volumeValue = document.getElementById('volume').value;
+    console.log(volumeValue);
+    video.volume = volumeValue;
+}
+
+function speedUp() {
+    let a = (video.playbackRate += 0.5);
     if (a > 4) {
-        alert('Limite de velocidade alcançado');
+        alert("Limite de velocidade alcançado");
         video.currentTime = 0;
     } else {
         spanVideo.innerHTML = a + "x";
     }
 }
-function speedDown(){
-    let a = video.playbackRate -= 0.5;
-    if (a <= 0){
+function speedDown() {
+    let a = (video.playbackRate -= 0.5);
+    if (a <= 0) {
         video.playbackRate = 1;
-        alert('O vídeo precisa ter velocidade para ser vizualizado');
+        alert("O vídeo precisa ter velocidade para ser vizualizado");
     } else {
-        spanVideo.innerHTML = a + "x"; 
+        spanVideo.innerHTML = a + "x";
     }
-    console.log(a);
 }
